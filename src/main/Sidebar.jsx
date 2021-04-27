a1a4946f45f06ded8172ed87210ee87571916604
lthrower5 <79727617+lthrower5@users.noreply.github.com>
2021-04-27 17:40:10 -0600

    Chat and weapons code/image added

--- src/main/Sidebar.jsx
+++ src/main/Sidebar.jsx
@@ -6,6 +6,35 @@ import MoveButtons from "./MoveButtons";
 import Cards from "./Cards";
 import Notes from "./Notes";
 
+import { CometChat } from "@cometchat-pro/chat";
+import { CometChatUI } from "./cometchat-pro-react-ui-kit/CometChatWorkspace/src";
+
+const appID = "319644fb8ab90c2";
+const region = "us";
+const appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(region).build();
+CometChat.init(appID, appSetting).then(
+  () => {
+    console.log("Initialization completed successfully");
+    // You can now call login function.
+  },
+  error => {
+    console.log("Initialization failed with error:", error);
+    // Check the reason for error and take appropriate action.
+  }
+);
+
+const authKey = "70f3097840cbc14022dad45cc692962f56107a9c";
+const uid = "SUPERHERO1";
+
+CometChat.login(uid, authKey).then(
+  user => {
+    console.log("Login Successful:", { user });    
+  },
+  error => {
+    console.log("Login failed with exception:", { error });    
+  }
+);
+
 class Sidebar extends Component {
     render() {
         return (
@@ -36,6 +65,9 @@ class Sidebar extends Component {
                 <Row>
                     <Notes/>
                 </Row>
+                <div style={{width: '600px', height:'600px' }}>
+                 <CometChatUI />
+                </div>
             </Container>
         );
     }
